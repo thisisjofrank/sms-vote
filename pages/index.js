2ab6fe98243a13c830ce947ffb4f55bd2d3cdc5b
thisisjofrank <jofranchetti@gmail.com>
2021-03-17 20:17:52 +0000

    update questions component

--- pages/index.js
+++ pages/index.js
@@ -2,15 +2,11 @@ import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 import dynamic from 'next/dynamic';
 
-const QuestionsComponent = dynamic(() => import('../components/QuestionsComponent'), { ssr: false });
+const QuestionsComponent = dynamic(() => import('../components/QuestionComponent'), { ssr: false });
 
-export async function getStaticProps(context) {
-  const response = await fetch('https://opentdb.com/api.php?amount=50&difficulty=medium&type=multiple');
-  const questions = (await response.json()).results;
-  return { props: { questions } };
-}
-
-export default function Home({ questions }) {
+export default function Home() {
+    
+  const question = getQuestion();
 
   return (
     <div className={styles.container}>
@@ -21,21 +17,30 @@ export default function Home({ questions }) {
       <main className={styles.main}>
       <header className={styles.header}>
         <img src="/images/smsvote.svg" alt="sms vote" className={styles.logo}/>
-        <h1 className={styles.title}>Text: +33 644 634 209 <br />to vote</h1>
+        <h1 className={styles.title}>Text: (+44) 7937 985 836 <br />to vote</h1>
       </header>
-        <QuestionsComponent questions={questions} />
+        <QuestionsComponent question={question} />
       </main>
 
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app" target="_blank" rel="noopener noreferrer">
           Powered by{' '}
           <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
         </a>
       </footer>
     </div>
   )
+}
+
+
+function getQuestion() {    
+  return {
+    text: "Which biscuit is best?",
+    options: [
+        { key: "A", text: "Jammy Dodger", votes: 0, src: "images/jamiedodger.jpg" },
+        { key: "B", text: "Oreo", votes: 0, src: "images/oreo.jpg" },
+        { key: "C", text: "Bourbon", votes: 0, src: "images/bourbon.jpg" },
+        { key: "D", text: "Custard cream", votes: 0, src: "images/custardcream.jpg" }
+    ]
+  };
 }
